074818c5a0bf4019338eef6f2e448898f4f590af
Tsuyoshi Yonemoto <tsuyoshi.yonemoto@gmail.com>
2014-07-08 23:04:35 +0900

    Add replacement pattern.

--- content.js
+++ content.js
@@ -1,11 +1,20 @@
 $('a').each(function(index) {
   var a_tag = this;
-  if($(this).text().match(/https:\/\/github\.com\/.*\/issues\/[0-9]*/)) {
-    var port = chrome.extension.connect();
-    port.postMessage({'url' : $(this).text()});
-    port.onMessage.addListener(function(msg) {
-      $(a_tag).text(msg.title);
-    });
+  if(!$(this).attr("href").match(/https:\/\/github\.com\/.*\/(pull|issues)\/[0-9]*/)) {
+    return;
   }
+  
+  var href_eq_title = $(this).text() == $(this).attr("href")
+  var short_title   = /.*#[0-9]+$/.test($(this).text())
+
+  if(!(href_eq_title || short_title)) {
+    return;
+  }
+  
+  var port = chrome.extension.connect();
+  port.postMessage({'url' : $(this).attr("href")});
+  port.onMessage.addListener(function(msg) {
+    $(a_tag).text(msg.title);
+  });
 });
 
