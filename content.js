5717634cc452cbea57d218f15a4356e61ab4945f
Tsuyoshi Yonemoto <tsuyoshi.yonemoto@gmail.com>
2017-03-05 11:10:17 +0900

    GitHubのコメントエリアにのみ適応するように変更

--- content.js
+++ content.js
@@ -7,23 +7,25 @@ document.addEventListener('DOMNodeInserted', () => {
   if(lock == true) { return }
   lock = true
 
-  Array.from(document.getElementsByTagName('a'), el => {
-    if(!el.href.match(/https:\/\/github\.com\/.*\/(pull|issues)\/[0-9]*/)) { return }
-    if(el.textContent == el.href)                                                 { return }
+  Array.from(document.getElementsByClassName('edit-comment-hide'), edits => {
+    Array.from(edits.getElementsByTagName('a'), el => {
+      if(!el.href.match(/https:\/\/github\.com\/.*\/(pull|issues)\/[0-9]*/)) { return }
+      if(el.textContent == el.href)                                                 { return }
 
-    if(titles[el.href] && el.textContent != titles[el.href]) {
-     el.textContent = titles[el.href]
-     return
-    }
+      if(titles[el.href] && el.textContent != titles[el.href]) {
+       el.textContent = titles[el.href]
+       return
+      }
 
-    if(skipUrls.indexOf(el.href) >= 0) { return }
-    skipUrls.push(el.href)
+      if(skipUrls.indexOf(el.href) >= 0) { return }
+      skipUrls.push(el.href)
 
-    const port = chrome.extension.connect()
-    port.postMessage({'url' : el.href})
-    port.onMessage.addListener(function(msg) {
-      el.textContent = msg.title
-      titles[el.href] = msg.title
+      const port = chrome.extension.connect()
+      port.postMessage({'url' : el.href})
+      port.onMessage.addListener(function(msg) {
+        el.textContent = msg.title
+        titles[el.href] = msg.title
+      })
     })
   })
 
